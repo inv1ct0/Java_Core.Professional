29562fde8e704197c1cabe60c650c5b57af9cd2f
inv1ct0 <a.m.yudenkov@yandex.ru>
2019-09-11 20:38:38 +0300

    les_6

---
+++ homeWork/src/main/java/com/inv1ct0/lesson_1/Apple.java
@@ -0,0 +1,7 @@
+package com.inv1ct0.lesson_1;
+
+class Apple extends Fruit {
+    Apple() {
+        super(1.0f);
+    }
+}
---
+++ homeWork/src/main/java/com/inv1ct0/lesson_1/Fruit.java
@@ -0,0 +1,13 @@
+package com.inv1ct0.lesson_1;
+
+abstract class Fruit {
+    private final float weight;
+
+    Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    float getWeight() {
+        return weight;
+    }
+}
